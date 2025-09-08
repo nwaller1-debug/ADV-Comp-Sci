25cf55b55f8716b52db1e66cbdf51f3d20743c48
Nick <nwaller1@hwemail.com>
2025-09-08 14:54:28 -0700

    Learned Equals method, toString

--- Day 0/Plant.java
+++ Day 0/Plant.java
@@ -10,6 +10,17 @@ public class Plant {
     private char Plantgrade; // = 'b';
     private int Plantage;
     
+    public Plant(int Plantage){
+        plants = 6;
+        PlantLocation = "in a pot";
+        PotExterior = "wool";
+        PotInside = "soil";
+        PlantDesc = "leaves on plant";
+        PlantHeight = 12.5f;
+        isAlive = true;
+        Plantgrade = 'b';
+    }
+
     public Plant() {
         plants = 6;
         PlantLocation = "in a pot";
@@ -52,6 +63,30 @@ public class Plant {
         System.out.println("the plant is now " + Plantage + "years old");
         if (Plantage > 100)
             System.out.println("You're plant is very old");
+    }
+    
+    public boolean equals(Plant otherPlant) {
+        if (
+            this.plants == otherPlant.plants &&
+            this.PlantHeight == otherPlant.PlantHeight &&
+            this.isAlive == otherPlant.isAlive &&
+            this.PlantDesc.equals(otherPlant.PlantDesc) &&
+            this.Plantage == otherPlant.Plantage
+            ) {
+                return true;
+            }
+        else{
+            return false;
+        }
+        }
+
+    public String toString() {
+        return "This plant is " + Plantage + " years old and is " + PlantLocation
+        + " and the plant is " + PlantHeight + " Inches tall ";
+    }
+
+    public static void main(){
+
     }
 
     // eat
--- Day 0/PlantTester.java
+++ Day 0/PlantTester.java
@@ -3,11 +3,31 @@ public class PlantTester {
     public static void main(String[] args) {
         System.out.println("yo whaddup!!!");
 
+        
         Plant p1plant = new Plant();
-        p1plant.die();
+        Plant p1plant2 = new Plant(5);
+        Plant chomper = new Plant();
+        if (p1plant.equals(p1plant2)){
+            System.out.println("They are the same");
+        }
+        else {
+            System.out.println("They are not the same");
+        }
+        chomper.drink();
+        chomper.getolder();
+        chomper.eat();
+        System.out.println(chomper.toString());
         p1plant.drink();
         p1plant.eat();
-        p1plant.fall();
         p1plant.getolder();
+        p1plant2.die();
+        System.out.println(p1plant2.toString());
+        System.out.println(p1plant.toString());
+        if (p1plant.equals(chomper)){
+            System.out.println("They are the same");
+        }
+        else {
+            System.out.println("They are not the same");
+        }
     }
 }
