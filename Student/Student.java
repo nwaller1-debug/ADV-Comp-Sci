6ce77aa603256b44493280f66d3bc933eae90707
Nick <nwaller1@hwemail.com>
2025-09-22 10:15:12 -0700

    Sep 22

--- Student/Student.java
+++ Student/Student.java
@@ -4,68 +4,69 @@ public class Student {
     private String id;
     private int grade;
 
-    public Student(String name, int grade){
+    public Student(String name, int grade) {
         this.name = name;
         this.grade = grade;
         this.id = generateId();
     }
 
-    public Student(String name){
+    public Student(String name) {
         this.name = name;
         this.grade = 10;
         this.id = generateId();
     }
     
-    public void setName(String n){
+    public void setName(String n) {
         name = n;
     }
 
-    public void setGrade(char g){
-        grade = g;
+    public void setGrade(int grade) {
+        this.grade = grade;
     }
 
-    public void setId(){
-        id = generateId();
+    public void setId(String id) {
+        this.id = id;
     }
 
-    public String getid(){
+    public String getId() {
         return id;
     }
     
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public int getGrade(){
+    public int getGrade() {
         return grade;
     }
 
 
-    public String generateId(){
-        int randomNum1 = (int)(Math.random() * 8 + 1);
-        int randomNum2 = (int)(Math.random() * 8 + 1);
-        int randomNum3 = (int)(Math.random() * 8 + 1);
-        int randomNum4 = (int)(Math.random() * 10);
-        int randomNum5 = (int)(Math.random() * 10);
-        int randomNum6 = (int)(Math.random() * 10);
-        return randomNum1 + randomNum2 + randomNum3 + "-" + randomNum4 + randomNum5 + randomNum6;
+    public String generateId() {
+        int randomNum1 = (int) (Math.random() * 8 + 1);
+        int randomNum2 = (int) (Math.random() * 8 + 1);
+        int randomNum3 = (int) (Math.random() * 8 + 1);
+        int randomNum4 = (int) (Math.random() * 10);
+        int randomNum5 = (int) (Math.random() * 10);
+        int randomNum6 = (int) (Math.random() * 10);
+        int randomNum7 = (int) (Math.random() * 10);
+        return "" + randomNum1 + randomNum2 + randomNum3 + "-" 
+            + randomNum4 + randomNum5 + randomNum6 + randomNum7;
     }
 
 
-    public String toString(){
+    public String toString() {
         return name + " is a " + grade + "th grade student. Their id is " + id;
     }
 
 
-    public boolean equals(Student otherStudent){
-        if(
-            this.name == otherStudent.name &&
-            this.grade == otherStudent.grade &&
-            this.id == otherStudent.id
+    public boolean equals(Student otherStudent) {
+        if (
+            this.name.equals(otherStudent.name)
+                && this.grade == otherStudent.grade
+                && this.id.equals(otherStudent.id)
         ) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
--- Student/StudentTester.java
+++ Student/StudentTester.java
@@ -1,7 +1,12 @@
 public class StudentTester {
     public static void main(String[] args) {
+        Student student0 = new Student("Nick", 10);
         Student student1 = new Student("Nick", 10);
         Student student2 = new Student("Atrain");
-        System.out.println(student1.toString());
+        student0.setId(student1.getId());
+        System.out.println(student2.toString());
+        System.out.println(student1.equals(student0));
+        System.out.println(student1.equals(student2));
 
+    }
 }
