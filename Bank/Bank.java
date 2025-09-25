afc032ec58e29e17af132774167e715f321c21b7
Nick <nwaller1@hwemail.com>
2025-09-25 14:27:31 -0700

    Added the extras for Bank.java

--- Bank/Bank.java
+++ Bank/Bank.java
@@ -38,6 +38,44 @@ public class Bank {
         }
     }
 
+    public double getEmployeeRatio(){
+        return (double) numberOfLoanOfficers / (double) numberOfTellers;
+    }
+
+    public void fireMembers(int numberToFire, boolean isLoanOfficer) {
+        if (isLoanOfficer == true && numberToFire > numberOfLoanOfficers) {
+            numberToFire = 0;
+        }
+        
+        if (isLoanOfficer == true && numberToFire > numberOfTellers) {
+            numberToFire = 0;
+        }
+        
+        if (
+            isLoanOfficer == true
+        ) {
+            numberOfLoanOfficers = numberOfLoanOfficers - numberToFire;
+        } else {
+            numberOfTellers = numberOfTellers - numberToFire;
+        }
+    }
+
+    public void transferEmployee(boolean isLoanOfficer) {
+        if (
+            isLoanOfficer == true
+        ) {
+            numberOfLoanOfficers = numberOfLoanOfficers - 1;
+            numberOfTellers = numberOfTellers + 1;
+        } else {
+            numberOfLoanOfficers = numberOfLoanOfficers + 1;
+            numberOfTellers = numberOfTellers - 1;
+        }
+    }
+
+    public double averageSalary() {
+        return (numberOfLoanOfficers * 75000 + numberOfTellers * 45000) / (double) computeTotalEmployees();
+    }
+    
     public void setNumberOfLoanOfficers(int numberOfLoanOfficers) {
         this.numberOfLoanOfficers = numberOfLoanOfficers;
     }
