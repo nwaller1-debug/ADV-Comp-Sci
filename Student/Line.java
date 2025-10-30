d5ab37d2e2862debaa42f7b722eaa0ced45f1888
Nick <nwaller1@hwemail.com>
2025-10-30 11:03:52 -0700

    10/30 pt 2

--- Student/Line.java
+++ Student/Line.java
@@ -2,11 +2,38 @@ public class Line {
     private int a;
     private int b;
     private int c;
+    private Point p1;
+    private Point p2;
 
+    public Line(Point p1, Point p2){
+        this.p1 = p1;
+        this.p2 = p2;
+        this.a = -(p2.getY() - p1.getY());
+        this.b = p2.getX() - p1.getX(); 
+        this.c = -(this.a * p1.getX() + p1.getY() * this.b); 
+    }
+    
     public Line(int a, int b, int c) {
         this.a = a;
         this.b = b;
         this.c = c;
+
+    }
+    
+    public void setP1(Point p1) {
+        this.p1 = p1;
+    }
+    
+    public void setP2(Point p2) {
+        this.p2 = p2;
+    }
+
+    public Point getP1() {
+        return p1;
+    }
+    
+    public Point getP2() {
+        return p2;
     }
     
     public void setA(int a) {
@@ -38,7 +65,9 @@ public class Line {
         return slope;
     }
 
-    public boolean isCoordinateOnLine(int x, int y) {
+    public boolean isCoordinateOnLine(Point p) {
+        int x = p.getX();
+        int y = p.getY();
         int line = (x * a) + (y * b) + c;
         if (
             line == 0
@@ -47,6 +76,21 @@ public class Line {
         } else {
             return false;
         }
+    }
+
+    public int calculateSlopeFormula() {
+        int x1 = p1.getX();
+        int x2 = p2.getX();
+        int y1 = p1.getY();
+        int y2 = p2.getY();
+        return (y2 - y1) / (x1 - x2);
+    }
+
+    public String generatePointSlopeFormula() {
+        int x1 = p1.getX();
+        int y1 = p1.getY();
+
+        return "(y - " + y1 + ") = m(x - )" + x1;
     }
 
     public String toString() {
--- Student/LineTester.java
+++ Student/LineTester.java
@@ -2,11 +2,15 @@ public class LineTester {
     public static void main(String[] args) {
         Line line1 = new Line(5, 4, -17);
         double slope1 = line1.calculateSlope(); 
-        boolean onLine1 = line1.isCoordinateOnLine(5, -2); 
         Line line2 = new Line(-25, 40, 30);
         double slope2 = line2.calculateSlope(); 
-        boolean onLine2 = line2.isCoordinateOnLine(5, -2); 
         System.out.println(line1.toString());
+        Point point1 = new Point(2, 07);
+        Point point2 = new Point(3, 01);
+        Point point3 = new Point(8, 10);
+        Point point4 = new Point(5, 2);
+        Line line3 = new Line(point1, point2);
+        
     }
 
 }
