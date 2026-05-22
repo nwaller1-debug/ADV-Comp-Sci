544e35d8d1c7550a15a693e94711a5c9081e7054
Nick <nwaller1@hwemail.com>
2026-05-22 14:58:22 -0700

    Update Situations.java

--- Final Project/Situations.java
+++ Final Project/Situations.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Situations {
@@ -6,9 +7,69 @@ public class Situations {
     private boolean inCave;
     private boolean isTaming;
     private Enemy enemy;
+    private ArrayList<AllyTame> allyList;
 
-    public Situations() {
-        System.out.println()
+    public Situations(Enemy enemy, ArrayList<AllyTame> ally) {
+        inSky = isInSky();
+        inCave = isInCave();
+        isTaming = involveTaming();
+        this.enemy = enemy;
+        this.allyList = ally;
+    }
+
+    public boolean isInSky() {
+        int finished = 0;
+        while (finished == 0) {
+            System.out.println("Will this situation be in the sky (y/n)");
+            Scanner scan = new Scanner(System.in);
+            String temp = scan.nextLine().toLowerCase();
+            scan.close();
+            if (temp.equals("y")) {
+                return true;
+            }
+            if (temp.equals("n")) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isInCave() {
+        int finished = 0;
+        while (finished == 0) {
+            System.out.println("Will this situation be in a cave (y/n)");
+            Scanner scan = new Scanner(System.in);
+            String temp = scan.nextLine().toLowerCase();
+            scan.close();
+            if (temp.equals("y")) {
+                return true;
+            }
+            if (temp.equals("n")) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean involveTaming() {
+        int finished = 0;
+        while (finished == 0) {
+            System.out.println("Will this situation involve taming or stealth");
+            Scanner scan = new Scanner(System.in);
+            String temp = scan.nextLine().toLowerCase();
+            scan.close();
+            if (temp.equals("y")) {
+                return true;
+            }
+            if (temp.equals("n")) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public ArrayList<AllyTame> recList() {
+
     }
 
 }
